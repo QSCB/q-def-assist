757991082b752b56704b70bb4bce2c337188e801
clintinbrummer4 <clintinbrummer4@gmail.com>
2024-10-10 20:01:34 +0000

    updated

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,6 +1,102 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
-    logs = {
+    logs 
+nix
+{ pkgs, ... }: {
+
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+
+    pkgs.nodejs_18
+  ];
+
+  # Sets environment variables in the workspace
+  env = {
+    SOME_ENV_VAR = "hello";
+  };
+
+  # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+  idx.extensions = [
+    "angular.ng-template"
+  ];
+
+  # Enable previews and customize configuration
+  idx.previews = {
+    enable = true;
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "start"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+          "--disable-host-check"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
+ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = with pkgs; [
+    nodejs_20
+    // Add other packages here.
+  ];
+
+  # Sets environment variables in the workspace
+  env = {
+    // Add environment variables here.
+  };
+
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "ms-vscode.vscode-typescript-next"
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        npm-install = "npm ci --no-audit --prefer-offline --no-progress --timing";
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = [ "index.html" "main.js" ];
+      };
+      # To run something each time the workspace is (re)started, use the `onStart` hook
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["npm" "run" "dev" "--" "--port" "$PORT"];
+          manager = "web";
+        };
+        conversation = {
+          command = ["./node_modules/.bin/ts-node", "src/server.ts"];
+          manager = "terminal";
+          waitFor = "Ready!";
+        };
+      };
+    };
+  };
+}
+   nodejs_18 = pkgs.nodejs_18.override {
+      enableV8Snapshot = true;
+      buildInputs = [ pkgs.openssl ];
+    };
+    packages = with pkgs; [
+      nodejs_18
+    ];
+ {
       enable = true;
     };{ pkgs, ... }: {
   # Which nixpkgs channel to use.
