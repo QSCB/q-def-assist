bfcf55f4066a131297fa8c7d375fae66c3c95d7c
clintinbrummer4 <clintinbrummer4@gmail.com>
2024-10-10 19:30:05 +0000

    updated

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,5 +1,8 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
+    logs = {
+      enable = true;
+    };
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:2936578596.
+l ~LicenseLog:2936578596.
 import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
