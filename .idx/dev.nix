303a5c284613a81eda25bcfe347ddacb7929c2a2
clintinbrummer4 <clintinbrummer4@gmail.com>
2024-10-10 19:33:53 +0000

    updated

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,7 +2,47 @@
 # see: https://developers.google.com/idx/guides/customize-idx-env
     logs = {
       enable = true;
+    };{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "ms-vscode.vscode-typescript-next"
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        npm-install = "npm ci --no-audit --prefer-offline --no-progress --timing";
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = [ "index.html" "main.js" ];
+      };
+      # To run something each time the workspace is (re)started, use the `onStart` hook
     };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["npm" "run" "dev" "--" "--port" "$PORT"];
+          manager = "web";
+        };
+        conversation = {
+          command = ["./node_modules/.bin/ts-node", "src/server.ts"];
+          manager = "terminal";
+          waitFor = "Ready!";
+        };
+      };
+    };
+  };
+}
+p
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
