9bbf0fed9f87adfc66770e15025e31fdf7afb179
clintinbrummer4 <clintinbrummer4@gmail.com>
2024-10-10 07:34:42 +0000

    updat

--- gemini-api-banner.js
+++ gemini-api-banner.js
@@ -6,6 +6,19 @@ export function maybeShowApiKeyBanner(key, action = `enter it at the top of
     banner.innerHTML = `
       To get started with the Gemini API,
       <a href="https://g.co/ai/idxGetGeminiKey" target="_blank">
+      get an API key</a> and ${action}`;
+    document.body.prepend(banner);
+  }
+}
+    banner.style.backgroundColor = 'green';
+export function maybeShowApiKeyBanner(key, action = `enter it at the top of
+<code>main.js</code>`) {
+  if (key === 'TODO') {
+    let banner = document.createElement('div');
+    banner.className = 'api-key-banner';
+    banner.innerHTML = `
+      To get started with the Gemini API,
+      <a href="https://g.co/ai/idxGetGeminiKey" target="_blank">
       get an API key</a> (Ctrl+Click) and ${action}`;
     document.body.prepend(banner);
   }
